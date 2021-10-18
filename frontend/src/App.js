ff6d9997f98294dc85b3a2c93e9b5ba236db9fe1
dongtony12 <dongtony0523@naver.com>
2021-10-18 20:28:42 +0900

    slack test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import Nav from "./Sidebar/Nav";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Lecture from './Pages/Lecture';
-import Profile from './Pages/Profile';
-import Header from './Component/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Lecture from "./Pages/Lecture";
+import Profile from "./Pages/Profile";
+import Header from "./Component/Header";
 import Logout from "./Pages/Logout";
 
 function App() {
   return (
-    
-    <Router>
-      <Header/>
-      <Nav />
-      <Switch>
-        <Route path="/Profile" component={Profile} />
-        <Route path="/" exact component={Home} />
-        <Route path="/Lecture" component={Lecture} />
-        <Route path="/Logout" component={Logout}/>
-      </Switch>
-    </Router>
+    <div>
+      test for slack
+      <Router>
+        <Header />
+        <Nav />
+        <Switch>
+          <Route path="/Profile" component={Profile} />
+          <Route path="/" exact component={Home} />
+          <Route path="/Lecture" component={Lecture} />
+          <Route path="/Logout" component={Logout} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 export default App;
