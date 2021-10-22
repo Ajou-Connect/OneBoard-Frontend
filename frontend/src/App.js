91b3915fb7285b82d094b22e2af6922be166b078
dongtony12 <dongtony0523@naver.com>
2021-10-23 04:42:24 +0900

    menu edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
-import Nav from "./Sidebar/Nav";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Profile from "./Pages/Profile";
-import MenuLecture from "./Pages/MenuLecture";
+// import Nav from "./Sidebar/Nav";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Home from "./Pages/Home";
+// import Profile from "./Pages/Profile";
+// import MenuLecture from "./Pages/MenuLecture";
 import Header from "./Component/Header";
+import SideBar from "./Component/SideBar";
 
 const App = () => {
   return (
     <div className="App-style">
       <Header />
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/MenuLecture" component={MenuLecture} />
-          <Route path="/" exact component={Home} />
-          <Route path="/Profile" component={Profile} />
-        </Switch>
-      </Router>
+      <SideBar />
     </div>
   );
 };
--- frontend/src/Pages/UserData.js
+++ frontend/src/Pages/UserData.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const UserData = [
   {
     name: "김동현",
