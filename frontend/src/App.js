f53bf830dd973a49238ee5927861f998b314bc71
dongtony12 <dongtony0523@naver.com>
2021-11-12 20:12:01 +0900

    add quill

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import './App.scss';
 import { Route } from 'react-router';
 import Routes from '../src/Component/Routes';
 import Main from './Pages/MainNav/Main';
+import Editor from './Pages/LecturePages/Notice/NoticeEditor';
 const App = () => {
   return (
     <div className="App-style">
-      <Route path="/main" component={Routes} />
-      <Route path="/Login" component={LoginCheck} />
-      <Route path="/" exact={true} component={Main} />
+      {/* <Route path="/main" component={Routes} />
+      <Route path="/Login" component={LoginCheck} /> */}
+      {/* <Route path="/" exact={true} component={Main} /> */}
+      <Editor />
     </div>
   );
 };
