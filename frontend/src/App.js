1ccb73135151bc464ea64e602b50d54b6be6b8f2
dongtony12 <dongtony0523@naver.com>
2021-11-15 03:29:20 +0900

    pdf fail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import LoginCheck from './Component/Login/LoginCheck';
-import './App.scss';
-import { Route } from 'react-router';
-import Routes from '../src/Component/Routes';
-import Main from './Pages/MainNav/Main';
-
+// import LoginCheck from './Component/Login/LoginCheck';
+// import './App.scss';
+// import { Route } from 'react-router';
+// import Routes from '../src/Component/Routes';
+// import Main from './Pages/MainNav/Main';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import MyDoc from './Pages/LecturePages/Plan/PdfTest';
 
 const App = () => {
   return (
     <div className="App-style">
-      <Route path="/main" component={Routes} />
+      {/* <Route path="/main" component={Routes} />
       <Route path="/Login" component={LoginCheck} />
-      <Route path="/" exact={true} component={Main} />
-      
+      <Route path="/" exact={true} component={Main} /> */}
+      <PDFDownloadLink document={<MyDoc />} fileName="somename.pdf">
+        {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
+      </PDFDownloadLink>
     </div>
   );
 };
