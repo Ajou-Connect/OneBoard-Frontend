889e6ccf37053e4bd2def8ab9bd5e2d250710e4f
dongtony12 <dongtony0523@naver.com>
2021-11-24 04:14:19 +0900

    main page edit

--- frontend/src/Pages/MainNav/Main.js
+++ frontend/src/Pages/MainNav/Main.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import mainImg from '../../img/MainImage2.png';
+import styled from 'styled-components';
+import './Main.scss';
+const Title = styled.div`
+  font-size: 30px;
+  margin-left: 1rem;
+  border-bottom: 1px solid #f7f9fc;
+  height: 40px;
+  line-height: 40px;
+  text-align: left;
+`;
 
 const Main = () => {
   return (
     <div>
-      OneBoard에 오신것을 환영합니다
-      <Link to="/Login">로그인 하러가기</Link>
+      <div className="into-main" style={{ width: '100%', height: '700px' }}>
+        <center>
+          <Link
+            to="/Login"
+            style={{
+              fontSize: '2rem',
+              cursor: 'pointer',
+              textDecoration: 'underline',
+              color: '#EDF3F8',
+            }}
+          >
+            로그인 하러가기
+          </Link>
+        </center>
+      </div>
     </div>
   );
 };
